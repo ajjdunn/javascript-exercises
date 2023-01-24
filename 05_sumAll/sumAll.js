f89766fd8027052f6b96d0ea4f791e4d0efa8c4d
ajjdunn <aaronjjdunn@gmail.com>
2023-01-24 14:42:50 -0800

    Completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,8 @@
-const sumAll = function(first, last) {
-if (first != Number) || (last != Number){
-    return "ERROR"
-} else {
-    
-}
+const sumAll = function (a, b) {
+  if (isNaN(a) || isNaN(b) || typeof a == "string" || typeof b == "string")
+    return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
+  return ((Math.abs(a - b) + 1) * (a + b)) / 2;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
