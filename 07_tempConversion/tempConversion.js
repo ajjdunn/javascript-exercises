2331dfa2c1d4021ac4678831d944c01205a4a503
ajjdunn <aaronjjdunn@gmail.com>
2023-01-27 11:21:02 -0800

    More TDD - .reduce is confusing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (x) {
+  let number = (x - 32) * 0.5555555555555556;
+  // let rounded = Math.round(number * 10) / 10;
+  // let fixed = rounded.toFixed(1);
+  return parseFloat(number.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+// x °F ≘ (x − 32) × 5/9 °C
+
+const convertToFahrenheit = function (x) {
+  let number = x * 1.8 + 32;
+  // let rounded = Math.round(number * 10) / 10;
+  // let fixed = rounded.toFixed(1);
+  return parseFloat(number.toFixed(1));
 };
+
+// x °C ≘ (x × 9/5 + 32) °F
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,23 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let sum = a - b;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function (x) {
+  let result = x.reduce((partialSum, a) => partialSum + a, 0);
+  return result;
 };
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +26,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
