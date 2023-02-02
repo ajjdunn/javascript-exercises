16a78bb3a2abd009df13f3cc39c626fca53327e2
ajjdunn <aaronjjdunn@gmail.com>
2023-02-02 12:59:07 -0800

    more googlin

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let regex = /[\W_]/g;
+  let lowRegStr = str.toLowerCase().replace(regex, "");
+  let reverseStr = lowRegStr.split("").reverse().join("");
+  return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+  const arr = [
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597,
+    2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025,
+  ];
+  if (x < 0) {
+    return "OOPS";
+  } else {
+    let result = arr[x];
+    return result;
+  }
 };
 
 // Do not edit below this line
