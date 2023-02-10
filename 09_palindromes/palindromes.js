d6833a280b9cf31397662b1fb04fee481aec5ec9
ajjdunn <aaronjjdunn@gmail.com>
2023-02-10 09:36:22 -0800

    More JS exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,5 @@
 const palindromes = function (str) {
+  // using the regen /[\W_]/g removes all non-alphanumeric characters
   let regex = /[\W_]/g;
   let lowRegStr = str.toLowerCase().replace(regex, "");
   let reverseStr = lowRegStr.split("").reverse().join("");
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,41 @@
-const caesar = function() {
+const caesar = function (str, amount) {
+  // Wrap the amount
+  if (amount < 0) {
+    return caesar(str, amount + 26);
+  }
 
+  // Make an output variable
+  var output = "";
+
+  // Go through each character
+  for (var i = 0; i < str.length; i++) {
+    // Get the character we'll be appending
+    var c = str[i];
+
+    // If it's a letter...
+    if (c.match(/[a-z]/i)) {
+      // Get its code
+      var code = str.charCodeAt(i);
+
+      // Uppercase letters
+      if (code >= 65 && code <= 90) {
+        c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+        // For test ("Hello, World!", 5)
+        // "H" = 72, 72-65 = 7. 7+5 = 12, 12 % 26 = 12, 12 + 65 = 77, 77 is "M"
+      }
+
+      // Lowercase letters
+      else if (code >= 97 && code <= 122) {
+        c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+      }
+    }
+
+    // Append
+    output += c;
+  }
+
+  // All done!
+  return output;
 };
 
 // Do not edit below this line
